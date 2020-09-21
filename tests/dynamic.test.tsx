c59ca1442a9f5c0087ea29c770821c612c3fe298
Gabe Klein <gabe@termtm.com>
2020-09-21 01:49:12 -0600

    improve tests formatting

--- tests/dynamic.test.tsx
+++ tests/dynamic.test.tsx
@@ -1,43 +1,42 @@
-import { render } from '@testing-library/react'
-import * as React from 'react'
+import { render, waitFor } from '@testing-library/react';
+import * as React from 'react';
 
-import { useVirtual } from '../src'
-import { Container, Inner, Row } from './components'
+import { useVirtual } from '../src';
+import { Container, Inner, Row } from './components';
+
+const App = () => {
+  const {
+    parentRef,
+    totalSize,
+    virtualItems
+  } = useVirtual({
+    size: 20,
+    overscan: 5,
+  })
+
+  return (
+    <Container ref={parentRef}>
+      <Inner style={{ height: `${totalSize}px` }}>
+        {virtualItems.map(row => (
+          <Row
+            key={row.index}
+            ref={row.measureRef}
+            style={{
+              height: `${row.size}px`,
+              transform: `translateY(${row.start}px)`,
+            }}>
+            Row {row.index}
+          </Row>
+        ))}
+      </Inner>
+    </Container>
+  )
+}
 
 it('should render given dynamic size', async () => {
-  function App() {
-    const {
-      parentRef,
-      totalSize,
-      virtualItems
-    } = useVirtual({
-      size: 20,
-      overscan: 5,
-    })
+  const rendered = render(<App />);
 
-    return (
-      <>
-        <Container ref={parentRef}>
-          <Inner style={{ height: `${totalSize}px` }}>
-            {virtualItems.map(row => (
-              <Row
-                key={row.index}
-                ref={row.measureRef}
-                style={{
-                  height: `${row.size}px`,
-                  transform: `translateY(${row.start}px)`,
-                }}
-              >
-                Row {row.index}
-              </Row>
-            ))}
-          </Inner>
-        </Container>
-      </>
-    )
-  }
-
-  const rendered = render(<App />)
-
-  rendered.getByText('Row 1')
+  await waitFor(() => {
+    rendered.getByText('Row 1')
+  })
 })
--- tests/render.test.tsx
+++ tests/render.test.tsx
@@ -1,42 +1,44 @@
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import * as React from 'react'
 
 import { useVirtual } from '../src'
 import { Container, Inner, Row } from './components'
 
+const App = () => {
+  const {
+    parentRef,
+    totalSize,
+    virtualItems
+  } = useVirtual({
+    size: 10000,
+    estimateSize: React.useCallback(() => 35, []),
+    overscan: 5,
+  })
+
+  // console.log(`Render sees ${virtualItems.length} items`)
+
+  return (
+    <Container ref={parentRef}>
+      <Inner style={{ height: `${totalSize}px` }}>
+        {virtualItems.map(row => (
+          <Row
+            key={row.index}
+            style={{
+              height: `${row.size}px`,
+              transform: `translateY(${row.start}px)`,
+            }}>
+            Row {row.index}
+          </Row>
+        ))}
+      </Inner>
+    </Container>
+  )
+}
+
 it('should render', async () => {
-  function App() {
-    const {
-      parentRef,
-      totalSize,
-      virtualItems
-    } = useVirtual({
-      size: 10000,
-      estimateSize: React.useCallback(() => 35, []),
-      overscan: 5,
-    })
+  const rendered = render(<App />);
 
-    return (
-      <>
-        <Container ref={parentRef}>
-          <Inner style={{ height: `${totalSize}px` }}>
-            {virtualItems.map(row => (
-              <Row
-                key={row.index}
-                style={{
-                  height: `${row.size}px`,
-                  transform: `translateY(${row.start}px)`,
-                }}>
-                Row {row.index}
-              </Row>
-            ))}
-          </Inner>
-        </Container>
-      </>
-    )
-  }
-
-  const rendered = render(<App />)
-
-  rendered.getByText('Row 1')
+  await waitFor(() => {
+    rendered.getByText('Row 1');
+  })
 })
