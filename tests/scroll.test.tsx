afae71f12661fbf62d18a5b2b3ce3046d8cfa84b
Gabe Klein <gabe@termtm.com>
2020-09-23 20:48:05 -0600

    recreate scroll test

--- tests/scroll.test.tsx
+++ tests/scroll.test.tsx
@@ -1,90 +1,61 @@
-// import { render } from '@testing-library/react'
-// import * as React from 'react'
+import { render, waitFor, fireEvent, act } from '@testing-library/react'
+import * as React from 'react'
 
-// import { VirtualController } from '../src'
-// import { Container, Inner, Row } from './components'
+import { useVirtual } from '../src'
+import { Container, Inner, Row } from './components'
 
-// const sleep = (time = 1000) => new Promise(r => setTimeout(r, time))
+const App = () => {
+  const {
+    parentRef,
+    totalSize,
+    virtualItems,
+    scrollToIndex
+  } = useVirtual({
+    size: 1000,
+    overscan: 5,
+    estimateSize(){
+      return 35;
+    },
+  });
 
-it.skip("", () => {})
+  return (
+    <>
+      <button 
+        data-testid="gotoRow50"
+        onClick={() => {
+          scrollToIndex(50)
+        }}>
+        Goto 50
+      </button>
+      <Container
+        ref={parentRef}>
+        <Inner
+          style={{ height: `${totalSize}px` }}>
+          {virtualItems.map(row => (
+            <Row
+              key={row.index}
+              style={{
+                height: `${row.size}px`,
+                transform: `translateY(${row.start}px)`,
+              }}>
+              Row {row.index}
+            </Row>
+          ))}
+        </Inner>
+      </Container>
+    </>
+  )
+}
 
-// it('scrolling utilities should work', async () => {
-//   function App() {
-//     const parentRef = React.useRef()
+it.skip('should render new rows when scrolled', async () => {
+  const rendered = render(<App />);
 
-//     const rowVirtualizer = VirtualController.using({
-//       size: 10000,
-//       parentRef,
-//       estimateSize: React.useCallback(() => 35, []),
-//       overscan: 5,
-//     })
+  await waitFor(() => {
+    rendered.getByText('Row 1');
+  })
 
-//     return (
-//       <>
-//         <Container ref={parentRef}>
-//           <Inner
-//             style={{
-//               height: `${rowVirtualizer.totalSize}px`,
-//             }}
-//           >
-//             {rowVirtualizer.virtualItems.map(virtualRow => (
-//               <Row
-//                 key={virtualRow.index}
-//                 style={{
-//                   height: `${virtualRow.size}px`,
-//                   transform: `translateY(${virtualRow.start}px)`,
-//                 }}
-//               >
-//                 Row {virtualRow.index}
-//               </Row>
-//             ))}
-//           </Inner>
-//         </Container>
-//         <button
-//           onClick={() => {
-//             rowVirtualizer.scrollToOffset(500)
-//           }}
-//         >
-//           scrollToOffset500
-//         </button>
-//         <button
-//           onClick={() => {
-//             rowVirtualizer.scrollToIndex(50)
-//           }}
-//         >
-//           scrollToIndex50
-//         </button>
-//       </>
-//     )
-//   }
-
-//   const rendered = render(<App />)
-
-//   await rendered.getByText('Row 1')
-
-//   act(() => {
-//     fireEvent.click(rendered.getByText('scrollToOffset500'))
-//     fireEvent.scroll(rendered.getByTestId('container'), {
-//       target: rendered.getByTestId('container'),
-//     })
-//     // await sleep()
-//   })
-
-//   await rendered.findByText('Row 20')
-//   await rendered.findByText('Row 8')
-
-//   act(() => {
-//     fireEvent.click(rendered.getByText('scrollToIndex50'))
-//     fireEvent.scroll(rendered.getByTestId('container'), {
-//       target: rendered.getByTestId('container'),
-//     })
-//     // await sleep()
-//   })
-
-//   await rendered.findByText('Row 62')
-//   await rendered.findByText('Row 50')
-
-//   // expect(rendered.getByTestId('container').scrollTop).toEqual(500)
-// })
-
-// fireEvent.scroll(scrollContainer, { target: { scrollY: 100 } });
+  act(() => {
+    const container = rendered.getByTestId("gotoRow50");
+    fireEvent.click(container);
+  })
+})
