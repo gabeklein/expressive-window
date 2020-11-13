6b2261790de4e0958253ee1dd85dfeaefc4ca458
Gabe Klein <gabe@termtm.com>
2020-11-12 22:16:05 -0700

    implement types

---
+++ index.d.ts
@@ -0,0 +1,60 @@
+import { VC } from "react-use-controller";
+
+export abstract class VirtualController extends VC {
+  /** Current size of virtual collection */
+  abstract size?: number;
+
+  /**
+   * Number of items to render past container bounds
+   * 
+   * Default: 0;
+   * */
+  abstract overscan?: number;
+
+  /** 
+   * Amount of padding between container and first item. (In pixels)
+   * 
+   * Default: 0;
+   * */
+  abstract paddingStart?: number;
+
+  /** 
+   * Amount of padding between container and last item. (In pixels)
+   * 
+   * Default: 0;
+   * */
+  abstract paddingEnd?: number;
+
+  /** 
+   * Flag list should scroll horizontally.
+   * 
+   * Default: false;
+   * */
+  abstract horizontal?: boolean;
+
+  /** Determines initial size to allocate before rendering a list element. */
+  abstract estimateSize?(forIndex: number): number;
+
+  /** Apply this reference to container element! */
+  readonly parentRef: {
+    current: HTMLElement | null;
+  }
+
+  /** Index and computed postion of all drawn containers */
+  readonly virtualItems: {
+    index: number;
+    start: number;
+    size: number;
+    end: number;
+    measureRef: (element: HTMLElement) => void;
+  }[]
+
+  /** May be overridden, but super() is required! */
+  didMount(): void;
+
+  /** Programatically scroll to specific offset. */
+  scrollToOffset(toOffset: number, opts: any): void;
+
+  /** Programatically scroll to specific item by index. */
+  scrollToIndex(index: number, opts?: any): void;
+}
