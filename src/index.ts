ca83514ea73aee7c33732aacb36c44fb6794d64c
Gabe Klein <gabe@termtm.com>
2021-01-05 00:09:30 -0500

    rename linear module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Core } from './controller';
 export { default as Virtual } from './virtual';
-export { default as Dynamic } from './linear';
+export { default as Dynamic } from './dynamic';
 export { default as Justified } from './justified';
