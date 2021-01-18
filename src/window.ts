c983b3a0bef6f43517cfcf13098f4af0e412b508
Gabe Klein <gabe@termtm.com>
2021-01-18 00:29:02 -0700

    improve HOC

--- src/window.ts
+++ src/window.ts
@@ -1,4 +1,4 @@
-import { FC, CSSProperties, createElement as create } from "react";
+import { FC, CSSProperties, createElement as create, ReactNode, useCallback } from "react";
 import Control, { Item } from "./controller";
 
 interface ComponentProps {
@@ -8,22 +8,26 @@ interface ComponentProps {
 
 interface ContainerProps {
   component: FC<ComponentProps>;
+  children?: ReactNode;
   style?: CSSProperties;
   className?: string;
 }
 
 export function WindowContainer(
-  props: ContainerProps, context: Control<any>){
+  props: ContainerProps, context: Control<Item>){
 
-  const { totalSize, container, render } = context.tap();
-  const { component, ...rest } = props;
-
-  const Item = ({ style, index, key = index }: Item) =>
-    create(component, { style, key, index })
+  const { totalSize, container, render, axis: [x] } = context.tap();
+  const { component, children, ...rest } = props;
+  const Child = useCallback((props: any) => {
+    return component(props, context); 
+  }, [component]);
 
   return (
     create("div", { ref: container, ...rest },
-      create("div", { style: { height: totalSize }},
-        render.map(Item)))
+      create("div", { style: { [x]: totalSize } },
+        children,
+        render.map(props => 
+          create(Child, props))
+      ))
   )
 }
