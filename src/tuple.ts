701ab75e6a4631bacdd1cb0a37e0f7793c6b86c5
Gabe Klein <gabe@expressive.dev>
2021-11-07 03:10:46 -0500

    constrain tuple instruction logic

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,37 +1,21 @@
 import Model, { set } from "@expressive/mvc";
 
-export function tuple<T extends any[]>(tuple: T): T;
-export function tuple<T extends any[]>(...args: T): T;
-export function tuple<T extends any[]>(): T | undefined;
-export function tuple<T extends any[]>(...args: T): T | undefined {
+export function tuple<T extends any[]>(...value: T): T | undefined {
   return set(
     function tuple(this: Model.Controller, key: string){
-      let current: T | undefined;
-  
-      if(args.length == 0)
-        current = undefined as any;
-      else if(args.length == 1 && typeof args[0] == "object")
-        current = args[0] as any;
-  
-      this.manage(key, args, (value: T) => {
+      this.manage(key, value, (next: T) => {
         let isNew = false;
-  
-        if(!current){
-          isNew = true;
-          current = value;
-        }
-        else // compare
-          for(const k in current)
-            if(current[k] !== value[k]){
-              current[k] = value[k];
-              isNew = true;
-            }
-  
-        if(isNew)
-          this.update(key, value);
+
+        for(const k in value)
+          if(value[k] !== next[k]){
+            value[k] = next[k];
+            isNew = true;
+          }
+
+        return isNew;
       })
   
-      return () => current;
+      return () => value;
     }
   );
 }
